684679436cf8249ac454357dcaf95c9e81ec1df7
AnujAroshA <anuja.piyadigama@multplx.com>
2014-11-13 11:38:42 +0530

    Initial Commit

---
+++ GitWithXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitWithXcode
+//
+//  Created by Anuja on 11/13/14.
+//  Copyright (c) 2014 AnujAroshA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitWithXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitWithXcode
+//
+//  Created by Anuja on 11/13/14.
+//  Copyright (c) 2014 AnujAroshA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
