0a00714da950c550d6160ac67f9d7d512eeb269c
AnujAroshA <anuja.piyadigama@multplx.com>
2014-11-13 12:13:01 +0530

    Add AppConstants files

---
+++ GitWithXcode/AppConstants.h
@@ -0,0 +1,15 @@
+//
+//  AppConstants.h
+//  GitWithXcode
+//
+//  Created by Anuja on 11/13/14.
+//  Copyright (c) 2014 AnujAroshA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppConstants : NSObject
+
+extern BOOL debugEnable;
+
+@end
---
+++ GitWithXcode/AppConstants.m
@@ -0,0 +1,15 @@
+//
+//  AppConstants.m
+//  GitWithXcode
+//
+//  Created by Anuja on 11/13/14.
+//  Copyright (c) 2014 AnujAroshA. All rights reserved.
+//
+
+#import "AppConstants.h"
+
+@implementation AppConstants
+
+BOOL debugEnable = YES;
+
+@end
